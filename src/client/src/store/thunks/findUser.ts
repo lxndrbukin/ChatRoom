0dd9e53efe2e867f5a649cb99a4bda11139db1b3
lxndrbukin <lxndrbukin@gmail.com>
2023-09-25 17:54:36 +0100

    Working on finding users via search

--- src/client/src/store/thunks/findUser.ts
+++ src/client/src/store/thunks/findUser.ts
@@ -2,6 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const findUser = createAsyncThunk('search/findUser', async (searchReq: string) => {
-  const res = await axios.post('/_api/users', { searchReq });
+  const res = await axios.get(`/_api/users?searchReq=${searchReq}`);
   return res.data;
 });
