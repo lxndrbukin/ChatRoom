2132e17c447c1e6032ec69ab0448b535ae24a908
lxndrbukin <alex.bukin@hotmail.com>
2023-10-08 22:01:13 +0100

    Working on adding friends functionality

--- src/client/src/store/thunks/getFriendsList.ts
+++ src/client/src/store/thunks/getFriendsList.ts
@@ -2,5 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const getFriendsList = createAsyncThunk('friends/getFriendsList', async () => {
-
+  const res = await axios.get('/_api/friend_requests');
+  return res.data;
 });
